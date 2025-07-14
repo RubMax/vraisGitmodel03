b104e9ede3dc9a69a8cd4e2d82c7c4884de5bb97
RubMax <54785672+RubMax@users.noreply.github.com>
2025-07-14 07:59:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-const SHEET_NAME = "ENTETE";
+const SHEET_NAME = "Produits";
 
-function doGet(e) {
-  if (e.parameter.page === "api") {
-    const data = getData();
-    return ContentService
-      .createTextOutput(JSON.stringify(data))
-      .setMimeType(ContentService.MimeType.JSON);
-  }
-
+/* Fetche obigatoire avec lien app scrip +?page=api */
+fetch('https://script.google.com/macros/s/AKfycbwoTyj8mpGYPfWCOxszGA-SPYTSBsJbJoHyFKgIr-b5xSAu-CO9pgE3bCebLGAWCVDnPg/exec?page=api')
+  .then(res => res.json())
+  /* **********important fonctio affich************* */
+  .then(displayProduits);
+  
+  
   return HtmlService
     .createHtmlOutputFromFile("index")
     .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+
+
+function include(filename) {
+  return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
 
-function getData() {
+/*function getData() {
   try {
+    ============== si cest diff ici il faut maudifier================
     const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET_NAME);
     const rows = sheet.getDataRange().getValues();
-    rows.shift(); // Enlève l'entête
+    rows.shift();
     return rows.map(r => ({
       section:     r[0],
       nom:         r[1],
@@ -26,10 +30,13 @@ function getData() {
       prix:        (typeof r[4] === 'number') ? r[4].toString().replace('.', ',') : (r[4] || ""),
       tailles:     r[5] || "",
       code:        r[6] || "",
-      pub:         r[7] || "",
-      pubInterval: isNaN(r[8]) ? 25000 : r[8] * 1000
+      pub:         r[7] || "", // Colonne H - contenu de la pub
+      pubInterval: isNaN(r[8]) ? 25000 : r[8] * 1000 // Colonne I - intervalle en secondes
     }));
   } catch (err) {
     throw new Error("Erreur getData: " + err.message);
   }
 }
+
+function saveData(data) {*/
+ 
